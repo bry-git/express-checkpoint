999e2aa515af34cc0a00b1b82267e8cbde474745
bryson crandall <bryson.crandall@yahoo.com>
2021-08-13 09:01:24 -0700

    query param by title is working

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const PORT = 3000;
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : '5432',
-      user : 'admin',
-      password : 'password',
-      database : 'postgres'
+        host: '127.0.0.1',
+        port: '5432',
+        user: 'admin',
+        password: 'password',
+        database: 'postgres'
     }
 })
 
@@ -21,48 +21,69 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 app.get('/', (req, res) => {
     res.send(200, 'this is root, use an endpoint')
-})  
+})
 
 app.get('/movies', (req, res) => {
-  knex
-    .select('*')
-    .from('movies')
-    .then(data => res.status(200).json(data))
-    .catch(err =>
-      res.status(404).json({
-        message:
-          'The data you are looking for could not be found. Please try again'
-      })
-    );
+
+    if(req.query.title) {
+        const title = req.query.title
+        knex
+        .select('*')
+        .from('movies')
+        .where({title: title})
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({
+                message:
+                    'The data you are looking for could not be found. Please try again'
+            })
+        );
+    }
+    else {
+        console.log(req.query)
+        knex
+        .select('*')
+        .from('movies')
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({
+                message:
+                    'The data you are looking for could not be found. Please try again'
+            })
+        );
+    }
 });
 
 app.get('/movies/:id', (req, res) => {
-    knex'
-      .select('*') //change query to by id
-      .from('movies')
-      .then(data => res.status(200).json(data))
-      .catch(err =>
-        res.status(404).json({
-          message:
-            'The data you are looking for could not be found. Please try again'
-        })
-      );
-  });
+    console.log(req.params.id)
+    const id = req.params.id
+    knex
+        .select('*')
+        .from('movies')
+        .where({ id: id })
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({
+                message:
+                    'The data you are looking for could not be found. Please try again'
+            })
+        );
+});
 
 app.post('/movies', (req, res) => {
     knex('movies')
-         .insert(req.body)
-         .then(() => res.send(200))
-         .catch(err =>
-            res.status(500))       
+        .insert(req.body)
+        .then(() => res.send(200))
+        .catch(err =>
+            res.status(500))
 })
 
 
 
 app.listen(PORT, () => {
-  console.log(`The server is running on ${PORT}`);
+    console.log(`The server is running on ${PORT}`);
 });
