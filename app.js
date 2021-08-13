22417c1f35dfd640ddab3cc55d362c0f39469b83
bryson crandall <bryson.crandall@yahoo.com>
2021-08-13 12:45:04 -0700

    API dynamically increments movie tables primary key

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-var bodyParser = require('body-parser')
 const app = express();
+const cors = require('cors')
 const PORT = 3000;
 
 const knex = require('knex')({
@@ -14,14 +14,9 @@ const knex = require('knex')({
     }
 })
 
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send(200, 'this is root, use an endpoint')
@@ -29,32 +24,32 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
 
-    if(req.query.title) {
+    if (req.query.title) {
         const title = req.query.title
         knex
-        .select('*')
-        .from('movies')
-        .where({title: title})
-        .then(data => res.status(200).json(data))
-        .catch(err =>
-            res.status(404).json({
-                message:
-                    'No Movie with that Title was found'
-            })
-        );
+            .select('*')
+            .from('movies')
+            .where({ title: title })
+            .then(data => res.status(200).json(data))
+            .catch(err =>
+                res.status(404).json({
+                    message:
+                        'No Movie with that Title was found'
+                })
+            );
     }
     else {
         console.log(req.query)
         knex
-        .select('*')
-        .from('movies')
-        .then(data => res.status(200).json(data))
-        .catch(err =>
-            res.status(404).json({
-                message:
-                    'The data you are looking for could not be found. Please try again'
-            })
-        );
+            .select('*')
+            .from('movies')
+            .then(data => res.status(200).json(data))
+            .catch(err =>
+                res.status(404).json({
+                    message:
+                        'The data you are looking for could not be found. Please try again'
+                })
+            );
     }
 });
 
@@ -75,26 +70,31 @@ app.get('/movies/:id', (req, res) => {
 });
 
 app.post('/movies', (req, res) => {
-    console.log(req.body)
-    knex('movies')
-        .insert({
-            id: req.body.id,
-            title: req.body.title,
-            genre: req.body.genre,
-            release_date: req.body.release_date,
-            created_at: req.body.created_at,
-            updated_at: req.body.updated_at
-        })
-        .then(() => res.send(200))
-        .catch(err =>
-            res.status(500))
+    knex('movies').max('id').then(data => {
+        if (data) {
+            let incrementedId = data[0].max;
+            incrementedId++
+            console.log('movieID', incrementedId)
+            knex('movies')
+                .insert({
+                    id: incrementedId,
+                    title: req.body.title,
+                    genre: req.body.genre,
+                    release_date: req.body.release_date,
+                    created_at: req.body.created_at,
+                    updated_at: req.body.updated_at
+                })
+                .then(data => {
+                    res.status(200).json(data)
+                })
+        }
+    })
 })
-
 
 app.delete('/movies/:id', (req, res) => {
     console.log(req.body)
     knex('movies')
-        .where({id: req.params.id})
+        .where({ id: req.params.id })
         .del()
         .then(() => res.send(200))
         .catch((err) => res.status(500))
